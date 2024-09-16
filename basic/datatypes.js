be22bbbf0fa4b3d0afde3a8c7cbcb3e1f18050b3
Mansi-Shahi2025 <166232766+Mansi-Shahi2025@users.noreply.github.com>
2024-09-16 16:42:33 +0000

    datatype

---
+++ basic/datatypes.js
@@ -0,0 +1,12 @@
+"use strict";//to use neweer version of js
+//alert(3+3) we are using node.js not browser so this will give error
+//number bigint null->standalone value 
+//undefined-it is diff from null,null represents empty value and undefined means value not defined
+//symbol-to find uniqueness
+
+
+
+//object
+console.log(typeof "ABS")
+console.log(typeof null)//null is an object
+console.log(typeof undefined)// it will return undefined it is specific datatype
