945ca25fda408e96a6acef07340577486a996b68
Mansi-Shahi2025 <166232766+Mansi-Shahi2025@users.noreply.github.com>
2024-09-16 17:04:25 +0000

    conver

---
+++ basic/typeconversion.js
@@ -0,0 +1,12 @@
+let score="33"
+let val=Number(score)
+console.log(typeof val)//coverted
+let s1="33ab"
+let val1=Number(s1)
+console.log(typeof val1)//conversion done but value not assigned
+console.log(val1)//it return Not a number nan
+let zer=null
+console.log(zer)//return 0
+let und=undefined
+console.log(und)//return nan
+// if want to convert a string not possible
