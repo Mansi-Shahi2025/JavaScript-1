3353343497d67d550540b0e3513f54ccf977403d
Mansi-Shahi2025 <166232766+Mansi-Shahi2025@users.noreply.github.com>
2024-10-09 17:18:31 +0000

    container config done

---
+++ basic/comparison.js
@@ -0,0 +1,8 @@
+console.log(null>0) //false
+console.log(null == 0); //false
+console.log(null >= 0);//true
+// comparison convert null to 0
+//== does not so 0 is not> 0 return false
+//>= returntrue
+//== return false
+//=== return true if datatype is also correct
